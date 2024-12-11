fe0cf06a5a50b77e44e70b95888ab6993e86a2b1
KimSeol-KR <kseol1017@gmail.com>
2024-12-11 23:26:34 +0900

    Add files via upload

--- tab.js
+++ tab.js
@@ -32,8 +32,36 @@ function buttonClick (btnNum){
 
     let count = $('.tab-button').length;
 
-    for (let btnNum = 0; btnNum < count; btnNum++){
-        $('.tab-button').eq(btnNum).on('click',function(){
-            buttonClick(btnNum);
-            });
-    }
+    // for (let btnNum = 0; btnNum < count; btnNum++){
+    //     $('.tab-button').eq(btnNum).on('click',function(){
+    //         buttonClick(btnNum);
+    //         });
+    // }
+
+
+    // $('.list').click(function(e){
+    //     // 지금누른게 버튼0 이면 탭열기(0)
+    //     if(e.target == document.querySelectorAll('.tab-button')[0]){
+    //         buttonClick(0);
+    //     }else if(e.target == document.querySelectorAll('.tab-button')[1]){
+    //         buttonClick(1);
+    //     }else if(e.target == document.querySelectorAll('.tab-button')[2]){
+    //         buttonClick(2);
+    //     }
+
+    // })
+
+    $('.list').click(function(e){ 
+    // 이벤트버블링 으로 인해 요소안에 하위요소가 있을 땐, 상위 요소를 선택하면 하위요소 까지 이벤트 감지
+       
+        buttonClick(e.target.dataset.id); 
+    //더 정확하게는 e.target.dataset.id 은 문자형 이라 정수형으로 바꾸면 됨 
+
+    })
+    // var 변수 = parseInt(문자);    //문자를 정수형 숫자로 변환해줌
+    // var 변수 = parseFloat(문자);     //문자를 실수형 숫자로 변환해줌
+    // var 변수 = Nember(문자);    //문자를 정수&실수형 숫자로 변환해줌
+
+    // 제이쿼리 함수를 이용하는 방법
+    // $(셀렉터).data('데이터이름', '값') 이렇게 저장하고
+    // $(셀렉터).data('데이터이름') 이렇게 출력합니다. 
